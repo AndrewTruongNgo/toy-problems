bc5b4cbcc4307c0271141db7f7a187c95afd2e4d
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-11-06 23:33:41 -0800

    Includes Stack DS

--- dataStructures/stack.js
+++ dataStructures/stack.js
@@ -1,9 +1,116 @@
-let word = 'racecar';
-let reversedWord = '';
-let storage = [];
+class MultiStack {
+	constructor(numStacks) {
+  	let array = new Array(numStacks * 5);
+    for (let i = 0; i < numStacks; i++) {
+    	array[i] = 0;
+    }
+    this.numStacks = numStacks;
+  }
 
-for (let i = 0; i < word.length; i++) {
-  storage.push(word[i]);
+  push(stackNum, val) {
+
+    let currentSize = this.size(stackNum);
+
+		let position = stackNum * 5 + this.numStacks + currentSize;
+    array[position] = val;
+
+    array[stackNum] += 1;
+  }
+
+  pop(stackNum) {
+    let currentSize = this.size(stackNum);
+
+    let position = stackNum * 5 * this.numStacks + currentSize - 1
+
+    let result = array[position]
+
+    array[stackNum] -= 1;
+
+		return result;
+
+  }
+
+  peek(stackNum) {
+
+    let currentSize = this.size(stackNum);
+
+    let position = stackNum * 5 * this.numStacks + currentSize - 1
+
+    let result = array[position]
+
+		return result;
+
+  }
+
+  size(stackNum) {
+    return array[stackNum];
+  }
 }
 
-for (let i = 0; i < storage.length; i )
+const LOAD_FACTOR = 0.7;
+
+class MultiStack {
+  constructor(numStacks) {
+    this.numStacks = numStacks;
+    this.capacity = 5;
+    this.array = new Array(numStacks * (this.capacity + 1));
+    for (let i = 0; i < numStacks; i++) {
+      this.array[i] = 0;
+    }
+  }
+
+  push(stackNum, value) {
+  	this.array[this.getIndex(stackNum, this.size(stackNum))] = value;
+    this.array[stackNum]++;
+    if (this.array[stackNum] / this.capacity >= LOAD_FACTOR) {
+      this.resize();
+    }
+  }
+
+  pop(stackNum) {
+    const retVal = this.array[this.getIndex(stackNum, this.size(stackNum) - 1)];
+    this.array[stackNum]--;
+    return retVal;
+  }
+
+  peek(stackNum) {
+    return this.array[this.getIndex(stackNum, this.size(stackNum) - 1)];
+  }
+
+  getIndex(stackNum, index, capacity = this.capacity) {
+    return numStacks + (stackNum * capacity) + index;
+  }
+
+  resize() {
+    const newArray = new Array(numStacks * (2 * this.capacity + 1));
+    for (let i = 0; i < numStacks; i++) {
+      newArray[i] = this.array[i];
+    }
+    for (let i = 0; i < numStacks; i++) {
+      for (let j = 0; j < this.size(i); j++) {
+        newArray[this.getIndex(i, j, this.capacity * 2)] = this.array[this.getIndex(i, j)];
+      }
+    }
+    this.array = newArray;
+  }
+
+  size(stackNum) {
+    return this.array[stackNum]
+  }
+}
+
+let x = [];
+
+/**
+const multiStack = new MultiStack(10);
+multiStack.push(0, 1);
+multiStack.push(0, 2);
+multiStack.push(2, 15);
+multiStack.push(2, 10);
+multiStack.size(2) // 2
+multiStack.peek(2) // 10
+multiStack.pop(2) // 10
+multiStack.size(2) // 1
+multiStack.peek(2) // 15
+multiStack.peek(7) // error
+*/
---
+++ linkedList.js
@@ -0,0 +1,72 @@
+/*
+ * Implement a linked list using the pseudoclassical instantiation pattern.
+ *
+ * Your linked list should have methods called "addToTail", "removeHead", and "contains."
+ *
+ */
+
+// EXAMPLE USAGE:
+// var list = new LinkedList();
+// list.tail;         //yields 'null'
+// list.addToTail(4);
+// list.addToTail(5);
+// list.head.value;   //yields '4';
+// list.contains(5);  //yields 'true';
+// list.contains(6);  //yields 'false';
+// list.removeHead(); //yields '4';
+// list.tail.value;   //yields '5';
+// list.removeHead(); //yields '5';
+// list.removeHead(); //yields 'null';
+
+
+var LinkedList = function() {
+  //fill me in!
+  this.head = null;
+  // this.tail = null;
+
+  let Node = (number) => {
+    this.value = number;
+    this.next = null;
+  }
+
+};
+
+//write methods here!
+
+LinkedList.prototype.addToTail = function(number) {
+  let node = new Node(number);
+
+  if (this.head === null) {
+    this.head = node;
+  } else {
+    while(this.head.next) {
+
+    }
+  }
+
+};
+
+LinkedList.prototype.removeHead = function() {
+
+};
+
+LinkedList.prototype.contains = function(number) {
+
+};
+
+LinkedList.prototype.makeNode = function() {
+
+};
+
+
+var list = new LinkedList();
+list.tail;         //yields 'null'
+list.addToTail(4);
+list.addToTail(5);
+list.head.value;   //yields '4';
+list.contains(5);  //yields 'true';
+list.contains(6);  //yields 'false';
+list.removeHead(); //yields '4';
+list.tail.value;   //yields '5';
+list.removeHead(); //yields '5';
+list.removeHead(); //yields 'null';
