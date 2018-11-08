e5bc08f46c256abef282b0e1df228cba6b8c407d
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-11-07 19:35:09 -0800

    Includes LinkedList

--- linkedList.js
+++ linkedList.js
@@ -22,40 +22,57 @@
 var LinkedList = function() {
   //fill me in!
   this.head = null;
-  // this.tail = null;
-
-  let Node = (number) => {
-    this.value = number;
-    this.next = null;
-  }
+  this.tail = null;
 
 };
 
 //write methods here!
 
 LinkedList.prototype.addToTail = function(number) {
-  let node = new Node(number);
+  let node = this.makeNode(number);
+  let currentNode = this.head;
 
   if (this.head === null) {
     this.head = node;
   } else {
-    while(this.head.next) {
-
+    while(currentNode.next) {
+      currentNode = currentNode.next;
     }
+    currentNode.next = node;
   }
 
 };
 
 LinkedList.prototype.removeHead = function() {
-
+  let currentNode = this.head;
+  if (!this.head) {
+    return null;
+  } else {
+    this.head = this.head.next;
+  }
+  return currentNode;
 };
 
 LinkedList.prototype.contains = function(number) {
+  let currentNode = this.head;
+
+  while (currentNode) {
+    if (currentNode.value === number) {
+      return true;
+    }
+      currentNode = currentNode.next;
+  }
+  return false;
 
 };
 
-LinkedList.prototype.makeNode = function() {
-
+LinkedList.prototype.makeNode = function(value) {
+  let Node = (number) => {
+    this.value = number;
+    this.next = null;
+  }
+  let node = Node(value);
+  return node;
 };
 
 
