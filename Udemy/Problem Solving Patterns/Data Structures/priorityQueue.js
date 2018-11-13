096db4dd3158c9c0ef1afdb42d36086222ce810b
Andrew Ngo <andrewtruongngo@gmail.com>
2018-11-12 21:21:35 -0800

    Includes priority queue

--- Udemy/Problem Solving Patterns/Data Structures/priorityQueue.js
+++ Udemy/Problem Solving Patterns/Data Structures/priorityQueue.js
@@ -9,15 +9,76 @@ class PriorityQueue {
     arr.push(newNode);
 
     let currIndex = arr.length - 1;
-    let elem = arr[currIndex];
+    let curr = arr[currIndex];
 
     while(currIndex > 0) {
-      let parentIndex = Math.floor(((currIndex - 1)/2))
+      let parentIndex = Math.floor(((currIndex - 1)/2));
+      let parent = arr[parentIndex];
+      if(curr.priority <= parent.priority) {
+        break;
+      }
+      arr[parentIndex] = curr;
+      arr[currIndex] = parent;
+      currIndex = parentIndex;
     }
   }
 
-  dequeue() {
 
+
+  dequeue() {
+    let arr = this.values;
+    let max = arr[0];
+    let curr = arr.pop();
+
+    if(arr.length < 1) {
+      return max;
+    }
+
+    arr[0] = curr;
+    let currIndex = 0;
+    let leftIndex = currIndex * 2 + 1;
+    let rightIndex = currIndex * 2 + 2;
+    let left = arr[leftIndex];
+    let right = arr[rightIndex];
+
+    while(curr.priority < left.priority || curr.priority < right.priority) {
+      if (left.priority > right.priority) {
+        arr[currIndex] = left;
+        arr[leftIndex] = curr;
+        currIndex = leftIndex;
+
+        curr = arr[currIndex];
+        leftIndex = currIndex * 2 + 1;
+        rightIndex = currIndex * 2 + 2;
+        left = arr[leftIndex];
+        right = arr[rightIndex];
+        if (!arr[leftIndex]) {
+          break;
+        }
+        if (!arr[rightIndex]) {
+          break;
+        }
+
+      } else {
+        arr[currIndex] = right;
+        arr[rightIndex] = curr;
+        currIndex = rightIndex;
+
+        curr = arr[currIndex];
+        leftIndex = currIndex * 2 + 1;
+        rightIndex = currIndex * 2 + 2;
+        left = arr[leftIndex];
+        right = arr[rightIndex];
+        if (!arr[leftIndex]) {
+          break;
+        }
+        if (!arr[rightIndex]) {
+          break;
+        }
+      }
+    }
+
+    return max;
   }
 
 
@@ -29,3 +90,10 @@ class Node {
     this.priority = priority
   }
 }
+
+let list = new PriorityQueue();
+list.enqueue('headache', 1);
+list.enqueue('booboo', 2);
+list.enqueue('owie', 3);
+
+list.enqueue('dead', 5);
