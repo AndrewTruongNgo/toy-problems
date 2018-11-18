abe3404e536e93cb607c610a7b53c3bc98fce461
Andrew Ngo <andrewtruongngo@gmail.com>
2018-11-17 16:27:32 -0800

    Includes async

--- promises.js
+++ promises.js
@@ -1,37 +1,61 @@
-let firstFunction = () => {
+// let firstFunction = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Hi my name is '), 2000);
+//   });
+// }
+//
+// let secondFunction = (dataFromFirst) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(dataFromFirst + 'Andrew')
+//   })
+// }
+//
+// firstFunction().then((data) => {
+//   return secondFunction(data);
+// }).then((data) => {
+//   console.log(data);
+// })
+
+
+// Promise and Async
+
+let test = (num) => {
+
+  let results = num * 90
+
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Hi my name is '), 2000);
-  });
-}
-
-let secondFunction = (dataFromFirst) => {
-  return new Promise((resolve, reject) => {
-    resolve(dataFromFirst + 'Andrew')
-  })
-}
-
-firstFunction().then((data) => {
-  return secondFunction(data);
-}).then((data) => {
-  console.log(data);
-})
-
-
-
-
-
-
-let test = (data) => {
-  return new Promise((resolve, reject) => {
-    let results = data * 10
-    if (typeof results === 'number') {
-      // Whatever that's in resolve will be 'data' in .then argument
-      resolve(results)
+    if (results === 900) {
+      console.log(results);
+      setTimeout( () => resolve(results), 2000);
     } else {
-      reject(results)
+      reject('This aint no number');
     }
   })
 }
 
-test(9).then((data) => console.log(data, 'success!'))
-.catch(() => console.log('nah fam'))
+let test2 = (num2) => {
+  let results = num2 / 2;
+  console.log(results);
+  return results;
+};
+
+
+//Regular Promise then and catch
+
+// test(10).then((data) => {
+//   console.log(data);
+//   return data;
+// })
+// .then((data) => console.log(data / 900))
+// .catch((err) => console.log(err));
+
+
+// Async magic
+
+let asyncTest = async() => {
+  let result = await test(10);
+  result2 = test2(result);
+  test2(result2);
+}
+
+asyncTest();
