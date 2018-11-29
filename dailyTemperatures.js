135c6749b0d646486f8616ac2b08170e7217df95
Andrew Ngo <andrewtruongngo@gmail.com>
2018-11-29 11:51:48 -0800

    Includes dailyTemperatures

---
+++ dailyTemperatures.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures = function(T) {
+    let results = [];
+
+    if (!T.length) {
+        return results;
+    }
+
+    for (let i = 0; i < T.length; i++) {
+        let days = 1;
+        let maxed = false
+        for (let j = i + 1; j < T.length; j++) {
+            let curr = T[i];
+            let next = T[j];
+            if (next > curr) {
+                results.push(days);
+                maxed = true;
+                break;
+            } else {
+                days++;
+            }
+        }
+        if (!maxed) {
+            results.push(0);
+        }
+    }
+
+    return results;
+};
