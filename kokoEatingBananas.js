1abc7091646cf6775889481e3c878f8e2a7769f2
Andrew Ngo <andrewtruongngo@gmail.com>
2018-12-01 20:43:25 -0800

    Includes kokoEatingBananas

---
+++ kokoEatingBananas.js
@@ -0,0 +1,38 @@
+// function sayHello() {
+//   console.log('Hello, World');
+// }
+
+// sayHello();
+// click 'run'
+
+/*
+3, 6, 7, 11   H =8//
+K =7
+
+Math.ceil(100/40)
+
+Input: piles = [100,11,23,4,20], H = 6
+
+K = 52
+
+currH <= H
+
+//Iterate through arr
+//Find max elem
+//Create start and end variables
+//K will be average of start and end
+
+//Create pointer for arr
+//Create currH counter
+//While currH <= H
+//If !arr[pointer] break
+//Subtract arr[pointer] with K, incrementing currH
+//If arr[pointer] <= 0, increment pointer
+
+//Compare currH with H
+//If currH <= H, then K is too high
+//Assign end as K and binary search for new K
+//Else K is too low
+//Assign start as K and binary search for new K
+
+*/
